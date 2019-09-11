89a4c1fa902a96ae3da1d0b9b31b30c30cd6beb4
oimo23 <oimo2338@gmail.com>
2019-09-12 01:37:13 +0900

    initial commit

---
+++ omikuji/Controller/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  omikuji
+//
+//  Created by 伏貫祐樹 on 2019/09/06.
+//  Copyright © 2019 yuki fushinuki. All rights reserved.
+//
+
+import UIKit
+import PopupDialog
+
+class ViewController: UIViewController {
+    
+    // おみくじデータをインスタンス化
+    private var omikuji = Omikuji()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
+    // 「引く」ボタンが押された
+    @IBAction func lotteryButton(_ sender: UIButton) {
+        
+        // 抽選を行う
+        omikuji.lottery()
+        
+        // ポップアップダイアログの中身を更新する
+        updatePopupUI()
+        
+    }
+    
+    // おみくじ結果を更新して表示
+    func updatePopupUI() {
+        
+        let title = "おみくじ結果"
+        let message = omikuji.result
+        // let image = UIImage(named: omikuji.decideOmikujiImage())
+        
+        // 設定した内容でポップアップダイアログを生成
+        let popup = PopupDialog(title: title, message: message)
+        
+        // ボタンを作成
+        let buttonOK = CancelButton(title: "OK") {}
+
+        // 作成したボタンを追加
+        popup.addButtons([buttonOK])
+        
+        // ダイアログを出す
+        self.present(popup, animated: true, completion: nil)
+        
+    }
+
+
+}
+
